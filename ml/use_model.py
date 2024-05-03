45ebc9fe5cf32777ee80a3dce7f5afcfd6c98b45
Robert Rittmuller <robert.rittmuller@dot.gov>
2024-05-03 17:24:33 +0000

    Refactored code to be more flexible in input data files

--- ml/use_model.py
+++ ml/use_model.py
@@ -1,110 +1,76 @@
 # Mobility Counts Prediction
-# 
+
 import pandas as pd
+import os
+import sys
+import pickle
 
 # Import custom modules
 import module_ai
 import module_data
 import setup_funcs
-import pickle
-import os
 
-def check_valid_file_path_env_var(var_name):
-    # Get the value of the environment variable
-    path = os.getenv(var_name)
-
-    # Check if the variable is set and has data
-    if path:
-        # Check if the path is a valid file
-        if os.path.isfile(path):
-            return True, path
-        else:
-            print(f"The path {path} does not point to a valid file.")
-            return False, None
-    else:
-        print(f"{var_name} is not set or is empty.")
-        return False, None
-
-# Don't run without knowing where the data is located! Set environment variable using "setx" in Windows, or "export" in linux.
-data_path_is_set, data_path = check_valid_file_path_env_var("TRIMS_DATA_SOURCE")
-if data_path_is_set:
-
-    # init ai module
+def main(data_path):
+    # Validate the provided data path
+    if not os.path.isfile(data_path):
+        print(f"The path {data_path} does not point to a valid file.")
+        return
+    
+    # Initialize AI and data modules
     ai = module_ai.ai()
-
-    # init data module
     source_data = module_data.data()
-    # Normalization Functions - DO NOT CHANGE FOR FUTURE USE
-    source_data.norm_functions = ['tmc_norm', 'tstamp_norm', 'startdate_norm', 'density_norm', 'time_before_after']
 
-    # setup data sources
-    cols1 = ['tmc_code',
-            'measurement_tstamp',
-            'average_speed_All',
-            'speed_All',
-            'data_density_All',
-            'data_density_Pass',
-            'data_density_Truck', 
-            'travel_time_seconds_All',
-            'start_latitude',
-            'start_longitude',
-            'end_latitude',
-            'end_longitude',
-            'miles',
-            'aadt',
-            'urban_code',
-            'thrulanes_unidir',
-            'f_system',
-            'route_sign',
-            'thrulanes',
-            'zip',
-            'Population_2022'
-            ]
+    # Normalization Functions - Set and do not change unless necessary
+    norm_functions = ['tmc_norm', 'tstamp_norm', 'startdate_norm', 'density_norm', 'time_before_after']
+    source_data.norm_functions = norm_functions
 
-    source_data.OUTPUT_FILE_PATH = str(data_path)
+    # Columns to use
+    # columns = [
+    #     'tmc_code', 'measurement_tstamp', 'average_speed_All', 'speed_All', 'data_density_All',
+    #     'data_density_Pass', 'data_density_Truck', 'travel_time_seconds_All', 'start_latitude',
+    #     'start_longitude', 'end_latitude', 'end_longitude', 'miles', 'aadt', 'urban_code',
+    #     'thrulanes_unidir', 'f_system', 'route_sign', 'thrulanes', 'zip', 'Population_2022'
+    # ]
+    columns = [
+        'tmc_code', 'measurement_tstamp', 'speed_All', 'data_density_All',
+        'data_density_Pass', 'data_density_Truck', 'travel_time_seconds_All', 'start_latitude',
+        'start_longitude', 'end_latitude', 'end_longitude', 'miles', 'aadt', 'urban_code',
+        'thrulanes_unidir', 'f_system', 'route_sign', 'thrulanes', 'zip'
+    ]
 
-    use_custom_cols = True # CHANGE TO TRUE IF USING COLUMNS ABOVE, OTHERWISE USING COLUMNS FROM module_data
-    #use_pkl = True
-	use_pt = True
+    # Setup data source path
+    source_data.OUTPUT_FILE_PATH = data_path
 
-	'''
-    # IF USING UPDATED DATASET (any new columns calculated or selected)
-    # make sure to delete existing norm_data.pkl otherwise old data will be loaded
-    if use_pkl and os.path.isfile("../norm_data.pkl"):
-        print("Loaded .pkl file")
-        source_data.dataset = pickle.load(open("../data/norm_data.pkl", "rb"))
+    use_custom_columns = True  # CHANGE TO FALSE IF NOT USING COLUMNS ABOVE
+
+    # Check and handle previously normalized data
+    norm_data_path = "../data/norm_data.pkl"
+    if os.path.isfile(norm_data_path):
+        print("Loading from cache (.pkl file)...")
+        source_data.dataset = pickle.load(open(norm_data_path, "rb"))
     else:
         source_data.read()
-        import pickle
-        pickle.dump(source_data.dataset, open("../data/norm_data.pkl", "wb"))
-	'''
+        with open(norm_data_path, "wb") as file:
+            pickle.dump(source_data.dataset, file)
 
-	# IF USING UPDATED DATASET (any new columns calculated or selected)
-    # make sure to delete existing norm_data.pkl otherwise old data will be loaded
-    if use_pt and os.path.isfile("../norm_data.pt"):
-        print("Loaded .pt file")
-        source_data.dataset = pickle.load(open("../data/norm_data.pt", "rb"))
-    else:
-        source_data.read()
-        import pickle
-        pickle.dump(source_data.dataset, open("../data/norm_data.pt", "wb"))
-
+    # Normalize the dataset
     normalized_df = source_data.normalized()
-    # If using custom columns, add the calculated columns to the custom cols1
-    # Otherwise, set columns equal to features training data set that already contains calculated_columns
-    if (use_custom_cols):
-        cols1.extend(source_data.calculated_columns)
-    else :
-        cols1 = source_data.features_training_set
+
+    # Use custom columns or the default set
+    if use_custom_columns:
+        columns.extend(source_data.calculated_columns)
+    else:
+        columns = source_data.features_training_set
+
     print(normalized_df)
 
-    # print(source_data.calculated_columns)
-    # print(cols1)
-
-    result = setup_funcs.train_model(ai, normalized_df, cols1, 'VOL')
-
-    # Test the final model
+    # Train and test the model
+    result = setup_funcs.train_model(ai, normalized_df, columns, 'VOL')
     print("Testing final model...")
-    print(setup_funcs.test_model(ai, normalized_df, cols1, 'VOL'))
+    print(setup_funcs.test_model(ai, normalized_df, columns, 'VOL'))
 
-    # TODO: calculate average percent diff between predictions and y_test 
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print("Usage: python script_name.py <data_file_path>")
+    else:
+        main(sys.argv[1])
